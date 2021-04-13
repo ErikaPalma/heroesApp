9754149128ca2609abb2226bfcd163afe7296ef2
Erika <erikapalmasan@gmail.com>
2021-04-13 12:00:54 +0200

    sidenav con Material

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
+
 import { AddHeroeComponent } from './views/add-heroe/add-heroe.component';
 import { BuscarComponent } from './views/buscar/buscar.component';
 import { HeroeDetailComponent } from './views/heroe-detail/heroe-detail.component';
@@ -15,6 +18,11 @@ import { HeroesListComponent } from './views/heroes-list/heroes-list.component';
     HomeComponent,
     HeroesListComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    HeroesRoutingModule,
+    FlexLayoutModule,
+  ],
 })
 export class HeroesModule {}
